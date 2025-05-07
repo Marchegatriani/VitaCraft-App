352c099f1b58140cbc536b159d9f91e354806231
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-05-07 12:26:51 +0800

    feat: OrganizationSection

--- src/app/cvbuilder/model/OrganizationSection.java
+++ src/app/cvbuilder/model/OrganizationSection.java
@@ -1,5 +1,25 @@
 package app.cvbuilder.model;
 
-public class OrganizationSection {
-    
+import java.util.List;
+
+public class OrganizationSection implements CVSection {
+    private List<OrganizationExperience> entries;
+
+    public OrganizationSection(List<OrganizationExperience> entries) {
+        this.entries = entries;
+    }
+
+    @Override
+    public String getSectionTitle() {
+        return "Organizational Experience";
+    }
+
+    @Override
+    public String getFormattedContent() {
+        StringBuilder sb = new StringBuilder();
+        for (OrganizationExperience org : entries) {
+            sb.append("- ").append(org.formatForDisplay()).append("\n");
+        }
+        return sb.toString();
+    }
 }
