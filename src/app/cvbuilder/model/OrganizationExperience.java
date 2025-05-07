4219da7744994749762610a1cf2b441f5ad98efb
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-05-07 12:24:46 +0800

    feat: OrganizationExperience

--- src/app/cvbuilder/model/OrganizationExperience.java
+++ src/app/cvbuilder/model/OrganizationExperience.java
@@ -1,5 +1,24 @@
 package app.cvbuilder.model;
 
-public class OrganizationExperience {
-    
+public class OrganizationExperience extends CVEntry {
+    private String role;
+    private String organization;
+    private String description;
+
+    public OrganizationExperience(String role, String organization, String start, String end, String description) {
+        super(start, end);
+        this.role = role;
+        this.organization = organization;
+        this.description = description;
+    }
+
+    @Override
+    public String formatForDisplay() {
+        return role + " at " + organization + " (" + getStart() + " - " + getEnd() + "): " + description;
+    }
+
+    @Override
+    public String toString() {
+        return formatForDisplay();
+    }
 }
