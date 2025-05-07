52fe1bcef33826d47573580fbaa777842750f97e
Ryannfirmansyah <ryannnfirmannsyah@gmail.com>
2025-05-07 21:52:24 +0800

    kode CVEntry

--- src/app/cvbuilder/model/CVEntry.java
+++ src/app/cvbuilder/model/CVEntry.java
@@ -1,5 +1,29 @@
 package app.cvbuilder.model;
 
-public class CVEntry {
-    
+public abstract class CVEntry {
+    protected String title;
+    protected String startDate;
+    protected String endDate;
+    protected String description;
+
+    public CVEntry(String title, String startDate, String endDate, String description) {
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    public abstract void display();
+
+    // Getters
+    public String getTitle() { return title; }
+    public String getStartDate() { return startDate; }
+    public String getEndDate() { return endDate; }
+    public String getDescription() { return description; }
+
+    // Setters
+    public void setTitle(String title) { this.title = title; }
+    public void setStartDate(String startDate) { this.startDate = startDate; }
+    public void setEndDate(String endDate) { this.endDate = endDate; }
+    public void setDescription(String description) { this.description = description; }
 }
