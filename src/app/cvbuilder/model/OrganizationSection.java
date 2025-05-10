95d503246fa19d734e70df2c24981ec1b3925005
HanifahBasir <hanifahatthahirahbasir@gmail.com>
2025-05-10 08:47:52 +0800

    edit organization & skill

--- src/app/cvbuilder/model/OrganizationSection.java
+++ src/app/cvbuilder/model/OrganizationSection.java
@@ -1,25 +1,8 @@
 package app.cvbuilder.model;
 
-import java.util.List;
-
-public class OrganizationSection implements CVSection {
-    private List<OrganizationExperience> entries;
-
-    public OrganizationSection(List<OrganizationExperience> entries) {
-        this.entries = entries;
+public class OrganizationSection extends CVSection<OrganizationExperience> {
+    public OrganizationSection() {
+        super("Organizational Experience");
     }
 
-    @Override
-    public String getSectionTitle() {
-        return "Organizational Experience";
-    }
-
-    @Override
-    public String getFormattedContent() {
-        StringBuilder sb = new StringBuilder();
-        for (OrganizationExperience org : entries) {
-            sb.append("- ").append(org.formatForDisplay()).append("\n");
-        }
-        return sb.toString();
-    }
 }
