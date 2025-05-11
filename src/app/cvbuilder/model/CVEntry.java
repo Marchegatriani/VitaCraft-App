6871658a393c0e72fdbebdc175691b5289f1d03b
Ryannfirmansyah <ryannnfirmannsyah@gmail.com>
2025-05-11 13:53:58 +0800

    kode lama cv entry & cv section

--- src/app/cvbuilder/model/CVEntry.java
+++ src/app/cvbuilder/model/CVEntry.java
@@ -1,27 +1,23 @@
-package app.cvbuilder.model;
+package model;
 
 public abstract class CVEntry {
-    protected String title;
-    protected String startDate;
-    protected String endDate;
-    protected String description;
+    private String start;
+    private String end;
 
-    public CVEntry(String title, String startDate, String endDate, String description) {
-        this.title = title;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.description = description;
+    public CVEntry(String start, String end) {
+        this.start = start;
+        this.end = end;
     }
 
-    public abstract void display();
+    public String getStart() {
+        return start;
+    }
 
-    public String getTitle() { return title; }
-    public String getStartDate() { return startDate; }
-    public String getEndDate() { return endDate; }
-    public String getDescription() { return description; }
+    public String getEnd() {
+        return end;
+    }
 
-    public void setTitle(String title) { this.title = title; }
-    public void setStartDate(String startDate) { this.startDate = startDate; }
-    public void setEndDate(String endDate) { this.endDate = endDate; }
-    public void setDescription(String description) { this.description = description; }
+    public String formatForDisplay() {
+        return "(" + start + " - " + end + ")";
+    } 
 }
